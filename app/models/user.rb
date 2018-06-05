8abc67968ada8a7f1a416a508a4e2cbb61832546
Zarek <simen1127@gmail.com>
2018-06-05 22:45:47 +0800

    add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :orders, dependent: :destroy       
+  has_many :orders, dependent: :destroy
+
+  def admin?
+    self.role == "admin"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "products#index"
-
   resources :products,only: [:index, :show]
+
+  namespace :admin do
+    root "products#index"
+  end
 
 end
